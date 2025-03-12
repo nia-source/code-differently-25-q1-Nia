a236d7d518595893be549b584b9a0adf327ef689
NelltheWiz <chutt251@gmail.com>
2025-03-12 16:45:19 -0400

    chore: adds Chanel's custom quiz (#137)

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { JasonWatsonQuiz } from './jason_watson_quiz.js';
-import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
+import { ChanelHuttQuiz } from './Chanel_Huttquiz.js';
 import { DavidAdenaikeQuiz } from './david_adenaike_quiz.js';
 import { EvanPhilakhongQuiz } from './evan_philakhong_quiz.js';
 import { EzraQuiz } from './ezra_quiz.js';
+import { JasonWatsonQuiz } from './jason_watson_quiz.js';
 import { Jbeyquiz } from './jbeyquiz.js';
+import { JeremiahWingQuiz } from './jeremiah_wing_quiz.js';
 import { KhaylaSaundersQuiz } from './khayla_quiz.js';
+import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
 import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
 import { RasheedMillerQuiz } from './rasheed_miller_quiz.js';
-import { JeremiahWingQuiz } from './jeremiah_wing_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -27,6 +28,7 @@ const QUIZ_PROVIDERS = [
   EvanPhilakhongQuiz,
   KhaylaSaundersQuiz,
   RasheedMillerQuiz,
+  ChanelHuttQuiz,
   JeremiahWingQuiz,
   JasonWatsonQuiz,
 ];
