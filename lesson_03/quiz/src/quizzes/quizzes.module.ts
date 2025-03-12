3ded8c6ab3216d92110348d0a67152e2e86d73f1
nia-source <npack251@gmail.com>
2025-03-12 16:31:25 +0000

    completed quiz

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
-import { Jbeyquiz } from './jbeyquiz.js';
-import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
 import { DavidAdenaikeQuiz } from './david_adenaike_quiz.js';
+import { Jbeyquiz } from './jbeyquiz.js';
+import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
+import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
+import { NiaPackquiz } from './nia_quiz.js';
 import { RasheedMillerQuiz } from './rasheed_miller_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
@@ -12,12 +13,13 @@ export const Quizzes = Symbol.for('Quizzes');
 // Add your quiz provider here.
 const QUIZ_PROVIDERS = [
   AnthonyMaysQuiz,
-  AnotherQuiz, 
-  MeikoStephensQuiz ,
+  AnotherQuiz,
+  MeikoStephensQuiz,
   MercedesMathewsQuiz,
   Jbeyquiz,
   DavidAdenaikeQuiz,
   RasheedMillerQuiz,
+  NiaPackquiz,
 ];
 
 @Module({
