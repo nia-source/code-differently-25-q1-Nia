8cf96e4ef3afe81ae2e0b544a1bf49d245ce2007
Khayla Saunders <kksaunders251@gmail.com>
2025-03-12 11:18:40 -0400

    chore: adds Khyla's custom quiz (#120)
    * Updated Less01 KS
    
    * Restored deleted styles and finalized assignment
    
    * lesson03quiz
    
    * Khayla Saunders fixed classname
    
    * feat: add a new quiz question to KhaylaSaundersQuiz
    
    * fix: rename KhaylaSaundersQuiz to khaylasaunders in quiz.yaml
    
    * fix: update answer choices to UNANSWERED in KhaylaSaundersQuiz
    
    * Corrected quiz yaml
    
    * feat: removed lesson 01
    
    * chore: removed extra line break.
    
    * chore: restore mistakenly deleted files
    
    ---------
    
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
-import { Jbeyquiz } from './jbeyquiz.js';
-import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
 import { DavidAdenaikeQuiz } from './david_adenaike_quiz.js';
+import { Jbeyquiz } from './jbeyquiz.js';
+import { KhaylaSaundersQuiz } from './khayla_quiz.js';
+import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
+import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
 import { RasheedMillerQuiz } from './rasheed_miller_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
@@ -17,6 +18,7 @@ const QUIZ_PROVIDERS = [
   MercedesMathewsQuiz,
   Jbeyquiz,
   DavidAdenaikeQuiz,
+  KhaylaSaundersQuiz,
   RasheedMillerQuiz,
 ];
 
