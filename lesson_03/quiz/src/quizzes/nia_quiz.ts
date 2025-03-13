a452230d47c13cfd6261b82892cec55a4403e575
nia-source <npack251@gmail.com>
2025-03-13 14:23:23 -0400

    feat: adds Nia's custom quiz (#141)
    * completed quiz
    
    * fix: fix quiz answers
    
    * chore: fixes code formatting
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: removes answers
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: fix bad character
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    ---------
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    Co-authored-by: Anthony D. Mays <anthony@morganlatimer.com>
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

---
+++ lesson_03/quiz/src/quizzes/nia_quiz.ts
@@ -0,0 +1,75 @@
+import {
+  AnswerChoice,
+  MultipleChoiceQuizQuestion,
+  QuizQuestion,
+  QuizQuestionProvider,
+} from 'codedifferently-instructional';
+
+export class NiaPackquiz implements QuizQuestionProvider {
+  getProviderName(): string {
+    return 'niapack';
+  }
+
+  makeQuizQuestions(): QuizQuestion[] {
+    return [
+      NiaPackquiz.makeQuestion0(),
+      NiaPackquiz.makeQuestion1(),
+      NiaPackquiz.makeQuestion2(),
+    ];
+  }
+
+  private static makeQuestion0(): QuizQuestion {
+    return new MultipleChoiceQuizQuestion(
+      0,
+      'What is branching? Why is it important?',
+      new Map<AnswerChoice, string>([
+        [
+          AnswerChoice.A,
+          'It is when a tree grows and is important to make the sky look beautiful',
+        ],
+        [
+          AnswerChoice.B,
+          'It is when you create a seperate copy of a code and is important because it doesnt affect the main version and makes it easier to work with a team',
+        ],
+        [
+          AnswerChoice.C,
+          'It is when you delete all previous versions of your code and is important so you dont have to start from scratch',
+        ],
+        [
+          AnswerChoice.D,
+          'It is merging all changes directly into the main code and is important because it speeds up develpoment by avoiding unnecessary review or debugging steps',
+        ],
+      ]),
+      AnswerChoice.UNANSWERED,
+    ); // Replace `UNANSWERED` with the correct answer.
+  }
+  private static makeQuestion1(): QuizQuestion {
+    return new MultipleChoiceQuizQuestion(
+      1,
+      'Which programming languages can you use in VS code?',
+      new Map<AnswerChoice, string>([
+        [AnswerChoice.A, 'Only Python'],
+        [AnswerChoice.B, 'Only JavaScript'],
+        [AnswerChoice.C, 'Multiple languages like Python, JavaScript, and C++'],
+        [AnswerChoice.D, 'C++'],
+      ]),
+      AnswerChoice.UNANSWERED,
+    ); // Replace `UNANSWERED` with the correct answer.
+  }
+  private static makeQuestion2(): QuizQuestion {
+    return new MultipleChoiceQuizQuestion(
+      2,
+      'What is a pull request request on GitHub?',
+      new Map<AnswerChoice, string>([
+        [
+          AnswerChoice.A,
+          'A way to merge changes from one branch into another after review',
+        ],
+        [AnswerChoice.B, 'A command to delete a branch permanently'],
+        [AnswerChoice.C, 'A method for creating a local  copy of a repository'],
+        [AnswerChoice.D, 'A tool to schedule automatic repository backups'],
+      ]),
+      AnswerChoice.UNANSWERED,
+    ); // Replace `UNANSWERED` with the correct answer.
+  }
+}
--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common';
 import { Bryanasingletonbarnhart } from './ bryana_singleton-barnhart_quiz.js';
+import { AnanatawaQuiz } from './ananatawa_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { DylanLaffertyQuiz } from './dylan_lafferty_quiz.js';
 import { ChanelHuttQuiz } from './Chanel_Huttquiz.js';
 import { DavidAdenaikeQuiz } from './david_adenaike_quiz.js';
 import { DavisDQuiz } from './davisd_quiz.js';
+import { DylanLaffertyQuiz } from './dylan_lafferty_quiz.js';
 import { EvanPhilakhongQuiz } from './evan_philakhong_quiz.js';
 import { EzraQuiz } from './ezra_quiz.js';
 import { JasonWatsonQuiz } from './jason_watson_quiz.js';
 import { Jbeyquiz } from './jbeyquiz.js';
-import { KarensQuiz } from './karen_alabi_quiz.js';
 import { JeremiahWingQuiz } from './jeremiah_wing_quiz.js';
+import { KarensQuiz } from './karen_alabi_quiz.js';
 import { KhaylaSaundersQuiz } from './khayla_quiz.js';
 import { MeikoStephensQuiz } from './meiko_stephens_quiz.js';
 import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
+import { MontezBradleyQuiz } from './montez_quiz.js';
+import { NiaPackquiz } from './nia_quiz.js';
 import { OliviaJamesQuiz } from './olivia_james_quiz.js';
-import { MontezBradleyQuiz } from './montez_quiz.js'; 
 import { RasheedMillerQuiz } from './rasheed_miller_quiz.js';
-import { AnanatawaQuiz } from './ananatawa_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -39,6 +40,7 @@ const QUIZ_PROVIDERS = [
   KhaylaSaundersQuiz,
   DylanLaffertyQuiz,
   RasheedMillerQuiz,
+  NiaPackquiz,
   DavisDQuiz,
   AnanatawaQuiz,
   OliviaJamesQuiz,
