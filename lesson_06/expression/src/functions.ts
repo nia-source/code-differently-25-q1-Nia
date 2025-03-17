73e5c18fba9132779d3c9748e7bd42262889b47e
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2025-03-17 13:11:56 -0400

    feat: adds lesson_06 homework & quiz, lesson_07 pre-work (#249)
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

---
+++ lesson_06/expression/src/functions.ts
@@ -0,0 +1,16 @@
+export const FUNCTIONS: Record<string, Function> = Object.freeze({
+  A: (a: number, b: number, c: number, d: number, e: number) =>
+    ((a + b) * c) / Math.pow(d, e),
+  B: (a: number, b: number, c: number, d: number, e: number) =>
+    Math.pow(a + b, c) / (d * e),
+  C: (a: number, b: number, c: number, d: number, e: number) =>
+    a / b + c * Math.pow(d, e),
+  D: (a: number, b: number, c: number, d: number, e: number) =>
+    (a * b) / Math.pow(c + d, e),
+  E: (a: number, b: number, c: number, d: number, e: number) =>
+    Math.pow((a + b) / c, d) * e,
+  F: (a: number, b: number, c: number, d: number, e: number) =>
+    (a * Math.pow(b + c, d)) / e,
+  G: (a: number, b: number, c: number, d: number, e: number) =>
+    ((Math.pow(a, b) + c) * d) / e,
+});
