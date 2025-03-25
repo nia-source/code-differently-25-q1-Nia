4c3094f5058af53b598f5338a62997390d51d7ce
nia-source <npack251@gmail.com>
2025-03-25 21:55:40 +0000

    feat: adds NiaPProvider class with column type mappings

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/NiaPProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/NiaPProvider.java
@@ -1,1 +1,24 @@
+package com.codedifferently.lesson9.dataprovider;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NiaPProvider extends DataProvider {
+  public String getProviderName() {
+    return "niap";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Boolean.class,
+        "column2", Integer.class,
+        "column3", Float.class,
+        "column4", Short.class,
+        "column5", Long.class,
+        "column6", Double.class,
+        "column7", String.class);
+  }
+}
 
