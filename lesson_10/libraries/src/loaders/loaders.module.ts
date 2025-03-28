7b3bd25441ace95f23357f28344eec7110953645
Tezz03 <105926982+Tezz03@users.noreply.github.com>
2025-03-28 15:15:22 -0400

    feat: adds Montez's media items loader (#364)
    Co-authored-by: “Tezz03” <“mmbradley@gmail.com”>

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
+import { MontezBLoader } from './montez_b_loaders.js';
 
 export const Loaders = Symbol.for('Loaders');
 
@@ -10,6 +11,7 @@ const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
   DylanLaffertysLoader,
   MercedesMathewsLoader,
+  MontezBLoader
 ];
 
 @Module({
