d5c20476643d9c5065d6c76d85303a10379fea3b
mercedes-mathews <89824585+mercedes-mathews@users.noreply.github.com>
2025-03-28 01:05:41 -0400

    feat: adds Mercedes' loader file with implemented loadMediaItems method (#359)
    * feat: adds loader file with implemented loadMediaItems method
    
    * fix: adjusted loadMediaItems variables
    
    * feat: implemented extra credit loadData function

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
+import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
-const LOADER_PROVIDERS = [AnthonyMaysLoader, DylanLaffertysLoader];
+const LOADER_PROVIDERS = [
+  AnthonyMaysLoader,
+  DylanLaffertysLoader,
+  MercedesMathewsLoader,
+];
 
 @Module({
   providers: [
---
+++ lesson_10/libraries/src/loaders/mercedes_mathews_loader.ts
@@ -0,0 +1,58 @@
+import csv from 'csv-parser';
+import fs from 'fs';
+import { Credit, MediaItem } from '../models/index.js';
+import { Loader } from './loader.js';
+
+export class MercedesMathewsLoader implements Loader {
+  getLoaderName(): string {
+    return 'mercedesmathews';
+  }
+
+  async loadData(): Promise<MediaItem[]> {
+    const credits = await this.loadCredits();
+    const mediaItems = await this.loadMediaItems();
+
+    const mediaMap = new Map<string, MediaItem>();
+    for (const media of mediaItems) {
+      mediaMap.set(media.getId(), media);
+    }
+
+    for (const credit of credits) {
+      const mediaItem = mediaMap.get(credit.getMediaItemId());
+      if (mediaItem) {
+        mediaItem.addCredit(credit);
+      }
+    }
+
+    console.log(
+      `Loaded ${credits.length} credits and ${mediaItems.length} media items`,
+    );
+
+    console.log(...mediaItems.values());
+    return [...mediaItems.values()];
+  }
+
+  async loadMediaItems(): Promise<MediaItem[]> {
+    const medias = [];
+    const readable = fs
+      .createReadStream('data/media_items.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { id, title, type, year } = row;
+      medias.push(new MediaItem(id, title, type, year, []));
+    }
+    return medias;
+  }
+
+  async loadCredits(): Promise<Credit[]> {
+    const credits = [];
+    const readable = fs
+      .createReadStream('data/credits.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { media_item_id, role, name } = row;
+      credits.push(new Credit(media_item_id, name, role));
+    }
+    return credits;
+  }
+}
