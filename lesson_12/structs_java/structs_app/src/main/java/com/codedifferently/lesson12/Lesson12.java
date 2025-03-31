7450dd4c5f17fb86f959d62adc5a69e2ed7b94ba
nia-source <npack251@gmail.com>
2025-03-31 21:04:52 +0000

    feat: implements gameResult method to determine winner based on linked list values

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,30 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+       if (head == null || head.next == null) {
+            return "No Pairs"; 
+        }
+
+        int oddPoints = 0, evenPoints = 0;
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val < current.next.val) {
+                oddPoints++;  
+            } else {
+                evenPoints++; 
+            }
+            current = current.next.next; 
+        }
+
+        
+        if (oddPoints > evenPoints) {
+            return "Odd";
+        } else if (evenPoints > oddPoints) {
+            return "Even";
+        } else {
+            return "Tie"; 
+        }
+    }
+
   }
-}
