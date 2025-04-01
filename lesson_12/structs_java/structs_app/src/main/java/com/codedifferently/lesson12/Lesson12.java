49d841fe2d4dbf2b57861e61475710fb860896ee
nia-source <npack251@gmail.com>
2025-04-01 15:25:18 +0000

    Nia_P refactor: improve formatting and readability of gameResult method

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,30 +7,32 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-       if (head == null || head.next == null) {
-            return "No Pairs"; 
-        }
-
-        int oddPoints = 0, evenPoints = 0;
-        ListNode current = head;
-
-        while (current != null && current.next != null) {
-            if (current.val < current.next.val) {
-                oddPoints++;  
-            } else {
-                evenPoints++; 
-            }
-            current = current.next.next; 
-        }
-
-        
-        if (oddPoints > evenPoints) {
-            return "Odd";
-        } else if (evenPoints > oddPoints) {
-            return "Even";
-        } else {
-            return "Tie"; 
-        }
+    if (head == null || head.next == null) {
+      return "No Pairs";
     }
 
+    int oddPoints = 0, evenPoints = 0;
+    ListNode current = head;
+
+    while (current != null && current.next != null) {
+
+      if (current.val < current.next.val) {
+
+        oddPoints++;
+
+      } else {
+
+        evenPoints++;
+      }
+      current = current.next.next;
+    }
+
+    if (oddPoints > evenPoints) {
+      return "Odd";
+    } else if (evenPoints > oddPoints) {
+      return "Even";
+    } else {
+      return "Tie";
+    }
   }
+}
