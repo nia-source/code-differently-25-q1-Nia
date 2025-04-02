5c2898001300643a393e7fd29f55c91043137273
NelltheWiz <chutt251@gmail.com>
2025-04-02 00:39:27 -0400

    feat: adds Chanel's media items loader and extra credit (#396)
    * feat: adds chanel_hutt_loader.ts
    
    * feat: completed loader.ts file
    
    * feat: completes loader and extra credit for lesson10

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { ChanelHuttLoader } from './chanel_hutt_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
 import { MontezBLoader } from './montez_b_loaders.js';
-
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
   DylanLaffertysLoader,
+  ChanelHuttLoader,
   MercedesMathewsLoader,
   MontezBLoader
 ];
