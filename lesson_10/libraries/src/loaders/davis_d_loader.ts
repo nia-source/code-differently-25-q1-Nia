4fe012ecd76370f8be749d52b7ffa14bd29f0bcc
DavisD251 <davisdwendell@gmail.com>
2025-04-02 21:25:58 -0400

    feat: add Davis' media items loader (#380)
    * feat: add lesson_10 HW
    
    * chore: added mini note for myself
    
    ---------
    
    Co-authored-by: Davis Darius <dwdavis251@gmail.com>
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

---
+++ lesson_10/libraries/src/loaders/davis_d_loader.ts
@@ -0,0 +1,48 @@
+import csv from 'csv-parser';
+import fs from 'fs';
+import { Credit, MediaItem } from '../models/index.js';
+import { Loader } from './loader.js';
+
+// Do the extra credit tonight as well as prework for tommorow!!
+
+export class DavisDLoader implements Loader {
+  getLoaderName(): string {
+    return 'davis';
+  }
+
+  async loadData(): Promise<MediaItem[]> {
+    const credits = await this.loadCredits();
+    const mediaItems = await this.loadMediaItems();
+
+    console.log(
+      `Loaded ${credits.length} credits and ${mediaItems.length} media items`,
+    );
+
+    return [...mediaItems.values()];
+  }
+
+  async loadMediaItems(): Promise<MediaItem[]> {
+    // TODO: Implement this method.
+    const MediaItems = [];
+    const readable = fs
+      .createReadStream('data/media_items.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { id, title, type, year } = row;
+      MediaItems.push(new MediaItem(id, title, type, year, []));
+    }
+    return MediaItems;
+  }
+
+  async loadCredits(): Promise<Credit[]> {
+    const credits = [];
+    const readable = fs
+      .createReadStream('data/credits.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { media_item_id, role, name } = row;
+      credits.push(new Credit(media_item_id, name, role));
+    }
+    return credits;
+  }
+}
--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { ChanelHuttLoader } from './chanel_hutt_loader.js';
 import { DavidAdenaikeLoader } from './david_adenaike_loader.js';
+import { DavisDLoader } from './davis_d_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
 import { MontezBLoader } from './montez_b_loaders.js';
@@ -11,6 +12,7 @@ export const Loaders = Symbol.for('Loaders');
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
   DylanLaffertysLoader,
+  DavisDLoader,
   ChanelHuttLoader,
   MercedesMathewsLoader,
   DavidAdenaikeLoader,
