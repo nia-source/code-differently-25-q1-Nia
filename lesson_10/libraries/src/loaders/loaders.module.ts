59b8e52b435dd408fbfd8e3a78f56a0eca78657a
Karen Alabi <117942705+karensprojects22@users.noreply.github.com>
2025-04-02 22:11:50 -0400

    feat: adds Karens's media items loader (#375)
    * feat: adds lKarens lesson_10 loader.ts file
    
    * chore: updated lesson_10 loader.modules.ts file
    
    * Update loaders.module.ts
    
    ---------
    
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -5,9 +5,11 @@ import { DavidAdenaikeLoader } from './david_adenaike_loader.js';
 import { DavisDLoader } from './davis_d_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { JBeyLoader } from './JBey_loader.js';
+import { KarenAlabiLoader } from './karen_alabi_loader.js';
 import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
 import { RasheedMillerLoader } from './rasheed_miller_loader.js';
 import { MontezBLoader } from './montez_b_loaders.js';
+
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
@@ -17,6 +19,7 @@ const LOADER_PROVIDERS = [
   ChanelHuttLoader,
   JBeyLoader,
   MercedesMathewsLoader,
+  KarenAlabiLoader,
   RasheedMillerLoader,
   DavidAdenaikeLoader,
   MontezBLoader
