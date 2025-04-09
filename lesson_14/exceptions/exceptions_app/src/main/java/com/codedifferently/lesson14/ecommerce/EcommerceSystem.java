2f94499a5aee7071d81f3e0eaccf223076c6dc97
nia-source <npack251@gmail.com>
2025-04-09 13:13:26 +0000

    feat: update EcommerceSystem to use final fields and improve order handling

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/EcommerceSystem.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/EcommerceSystem.java
@@ -5,8 +5,8 @@ import java.util.Map;
 import java.util.UUID;
 
 public class EcommerceSystem {
-  private Map<String, Product> products;
-  private Map<String, Order> orders;
+  private final Map<String, Product> products;
+  private final Map<String, Order> orders;
 
   public EcommerceSystem() {
     products = new HashMap<>();
@@ -19,6 +19,8 @@ public class EcommerceSystem {
 
   public String placeOrder(String productId, int quantity) {
     Product product = products.get(productId);
+    if (product == null) {
+    } 
     String orderId = UUID.randomUUID().toString();
     orders.put(orderId, new Order(orderId, product, quantity));
     return orderId;
@@ -28,8 +30,11 @@ public class EcommerceSystem {
     orders.remove(orderId);
   }
 
-  public String checkOrderStatus(String orderId) {
+  public String checkOrderStatus(String orderId) throws IllegalArgumentException {
     Order order = orders.get(orderId);
+    if (order == null) {
+      return "Order not found";
+    }
     return "Order ID: "
         + orderId
         + ", Product: "
